79c0c717ba2539081cce9c363d4c48a557a2b3ae
Joey Sanchez <joeysanchez@mail.weber.edu>
2021-02-25 22:12:14 -0700

    Everything is working but somtimes it doesnt connection to the DB successfully.

--- utilities/connection.php
+++ utilities/connection.php
@@ -1,13 +1,9 @@
 <?php
 $servername = "testdbshows.mysql.database.azure.com";
-//$username = "Joey@testdbshows";
-//$password = "Metobe3!";
+
 $username = (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
 $password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
 $dbname = "testdb";
-
-
-
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
