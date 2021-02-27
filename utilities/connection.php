806bb25c46077935679612c14caed29e11802779
Joey Sanchez <joeysanchez@mail.weber.edu>
2021-02-26 21:00:35 -0700

    changed to user schemca

--- utilities/connection.php
+++ utilities/connection.php
@@ -4,7 +4,9 @@ $username = "Joey@testdbshows";
 $password = "Metobe3!";
 //$username = (isset($_SESSION["SQLUSER"]) ? $_SESSION["SQLUSER"] : $_ENV['SQLUSER']);
 //$password = (isset($_SESSION["SQLPW"]) ? $_SESSION["SQLPW"] : $_ENV['SQLPW']);
-$dbname = "testdb";
+//$dbname = "testdb";
+$dbname = "user_table";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
