c76b9306b0c6a2e1d203ff7b1df98e7c406514a0
Joey Sanchez <joeysanchez@mail.weber.edu>
2021-02-26 21:02:44 -0700

    wevwbvwebwrbwrb

--- utilities/connection.php
+++ utilities/connection.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $servername = "testdbshows.mysql.database.azure.com";
 $username = "Joey@testdbshows";
 $password = "Metobe3!";
