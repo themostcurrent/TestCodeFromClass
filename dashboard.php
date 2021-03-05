f91f761170e15ca4a1f77d378f28032561124f9d
Joey Sanchez <joeysanchez@mail.weber.edu>
2021-03-04 23:42:52 -0700

    jnfjbebvwjbver

--- dashboard.php
+++ dashboard.php
@@ -3,8 +3,9 @@
     <!-- Begin page content -->
     <main role="main" class="container">
       <h1 class="mt-5">Joey's Favorite Shows</h1>
-      <a href="./create_show.php" class="btn-primary form-control btn">Add New show</a>
-
+      <div class="col-6">
+        <a asp-page="./Index" class="btn-primary form-control btn">Back to List</a>
+      </div>
       <?php
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
