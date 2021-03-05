0cc8253527802891e28f12ce99267ff433864969
Joey Sanchez <joeysanchez@mail.weber.edu>
2021-03-04 22:16:23 -0700

    drbetnetn

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,50 @@
-<?php require_once("sessioncheck.php") ?>
+<?php require_once('header.php'); ?>
 
-I am protected
+    <!-- Begin page content -->
+    <main role="main" class="container">
+      <h1 class="mt-5">Joey's Favorite Shows</h1>
+      
 
-<a href="logout.php">Logout</a>
+      <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+
+        require_once('./show/show.php');
+
+        $show = new show();
+        $shows = $show->getMyShows();  
+
+        $arrlength = count($shows);
+
+        for($x = 0; $x < $arrlength; $x++) {            
+            echo '<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">' . $shows[$x]->getShowName() . '</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Rating: ' . $shows[$x]->getShowRating() . '</h6>
+                        <p class="card-text">' . $shows[$x]->getShowDescription() . '</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                  </div>
+                  <br />';
+        }
+      ?>
+
+
+      <!--
+     <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+        </div>
+     </div>
+    -->
+
+    </main>
+
+<?php require_once('footer.php'); ?>
+© 2021 GitHub, Inc.
---
+++ show/showDAO.php
@@ -0,0 +1,33 @@
+<?php
+class ShowDAO {
+  function getAllShows(){
+    require_once('./utilities/connection.php');
+    require_once('./show/show.php');
+
+    $sql = "SELECT showId, showName, rating, analysis, userId FROM shows_table.shows";
+    $result = $conn->query($sql);
+
+    $shows;
+    $index = 0;
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $show = new show();
+
+            $user->setShowId($row["showId"]);
+            $user->setShowName($row["showName"]);
+            $user->setRating($row["rating"]);
+            $user->setAnalysis($row["analysis"]);
+            $user->setUserId($row["userId"]);
+            $shows[$index] = $show;
+            $index = $index + 1;
+        }
+    } 
+    $conn->close();
+
+    return $shows;
+  }
+}//endclass
+?>
+
